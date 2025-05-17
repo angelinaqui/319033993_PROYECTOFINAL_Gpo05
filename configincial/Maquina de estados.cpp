3562247be829eec263021ad0dfd54463c95cc6a6
ANGEL IÑAQUI DIAZ GONZALEZ RIVAS <angeldg495@gmai.com>
2025-05-17 16:54:55 -0600

    Subiendo todos los archivos del proyecto

---
+++ configincial/Maquina de estados.cpp
@@ -0,0 +1,585 @@
+#include <iostream>
+#include <cmath>
+
+// GLEW
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+// Other Libs
+#include "stb_image.h"
+
+// GLM Mathematics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+//Load Models
+#include "SOIL2/SOIL2.h"
+
+
+// Other includes
+#include "Shader.h"
+#include "Camera.h"
+#include "Model.h"
+
+// Function prototypes
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void MouseCallback(GLFWwindow* window, double xPos, double yPos);
+void DoMovement();
+void Animation();
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+int SCREEN_WIDTH, SCREEN_HEIGHT;
+
+// Camera
+Camera  camera(glm::vec3(0.0f, 0.0f, 3.0f));
+GLfloat lastX = WIDTH / 2.0;
+GLfloat lastY = HEIGHT / 2.0;
+bool keys[1024];
+bool firstMouse = true;
+// Light attributes
+glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+bool active;
+
+// Positions of the point lights
+glm::vec3 pointLightPositions[] = {
+	glm::vec3(0.0f,10.0f, 0.0f),
+	glm::vec3(0.0f,0.0f, 0.0f),
+	glm::vec3(0.0f,0.0f,  0.0f),
+	glm::vec3(0.0f,0.0f, 0.0f)
+};
+
+float vertices[] = {
+	 -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	   -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	   -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+	   -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+	   -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+	   -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+	   -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+	   -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+	   -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+	   -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+	   -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+	   -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+	   -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+	   -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+	   -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+	   -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+	   -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+	   -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+};
+
+
+
+glm::vec3 Light1 = glm::vec3(0);
+//Anim
+glm::vec3 ventcPos(0.0f, 0.0f, 0.0f);  // posición inicial
+bool ventcMovingRight = false;          // si se está moviendo a la derecha
+bool ventcReturning = false;             // si está regresando
+const float ventcMaxMove = 0.21f;         // distancia máxima que se moverá a la derecha
+const float ventcSpeed = 0.001f;           // velocidad del movimiento
+
+glm::vec3 ventPos(-1.05f, 1.82f, -12.41f);  // posición base (fija)
+bool ventRotatingForward = false;  // si está rotando hacia 90°
+bool ventRotatingBackward = false; // si está regresando a 0°
+float ventRotationAngle = 0.0f;    // ángulo actual de rotación
+const float ventRotationSpeed = 0.1f;   // velocidad de rotación (grados por frame)
+const float ventRotationMax = 90.0f;    // ángulo máximo a rotar (90 grados)
+
+
+glm::vec3 puertPos(0.72f, 1.14f, -8.85f);  // posición inicial
+bool puertRotatingForward = false;
+bool puertRotatingBackward = false;
+float puertRotationAngle = 0.0f;
+const float puertRotationSpeed = 0.5f; // ajusta velocidad
+const float puertRotationMax = 90.0f;  // grados (negativo en la rotación)
+
+glm::vec3 compuPos(2.4f, 1.37f, -7.79f);  // posición inicial
+bool compuRotatingForward = false;
+bool compuRotatingBackward = false;
+float compuRotationAngle = 0.0f;
+const float compuRotationSpeed = 0.5f; // ajusta velocidad
+const float compuRotationMax = 95.0f;  // grados
+
+
+// Deltatime
+GLfloat deltaTime = 0.0f;	// Time between current frame and last frame
+GLfloat lastFrame = 0.0f;  	// Time of last frame
+
+int main()
+{
+	// Init GLFW
+	glfwInit();
+	// Set all the required options for GLFW
+	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);*/
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Animacion maquina de estados", nullptr, nullptr);
+
+	if (nullptr == window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
+
+	// Set the required callback functions
+	glfwSetKeyCallback(window, KeyCallback);
+	glfwSetCursorPosCallback(window, MouseCallback);
+
+	// GLFW Options
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Define the viewport dimensions
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+
+
+	Shader lightingShader("Shader/lighting.vs", "Shader/lighting.frag");
+	Shader lampShader("Shader/lamp.vs", "Shader/lamp.frag");
+
+	//models
+	Model compu((char*)"Models/compu1.obj");
+	Model vent((char*)"Models/ventana1.obj");
+	Model ventc((char*)"Models/ventcuart.obj");
+	Model puert((char*)"Models/puerta1.obj");
+	Model ran((char*)"Models/ran.obj");
+
+
+
+
+
+	// First, set the container's VAO (and VBO)
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Set texture units
+	lightingShader.Use();
+	glUniform1i(glGetUniformLocation(lightingShader.Program, "Material.difuse"), 0);
+	glUniform1i(glGetUniformLocation(lightingShader.Program, "Material.specular"), 1);
+
+	glm::mat4 projection = glm::perspective(camera.GetZoom(), (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 100.0f);
+
+	// Game loop
+	while (!glfwWindowShouldClose(window))
+	{
+
+		// Calculate deltatime of current frame
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+		DoMovement();
+		Animation();
+
+		// Clear the colorbuffer
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// OpenGL options
+		glEnable(GL_DEPTH_TEST);
+
+
+		glm::mat4 modelTemp = glm::mat4(1.0f); //Temp
+
+
+
+		// Use cooresponding shader when setting uniforms/drawing objects
+		lightingShader.Use();
+
+		glUniform1i(glGetUniformLocation(lightingShader.Program, "diffuse"), 0);
+		//glUniform1i(glGetUniformLocation(lightingShader.Program, "specular"),1);
+
+		GLint viewPosLoc = glGetUniformLocation(lightingShader.Program, "viewPos");
+		glUniform3f(viewPosLoc, camera.GetPosition().x, camera.GetPosition().y, camera.GetPosition().z);
+
+
+		// Directional light
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "dirLight.direction"), -0.2f, -1.0f, -0.3f);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "dirLight.ambient"), 0.6f, 0.6f, 0.6f);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "dirLight.diffuse"), 0.6f, 0.6f, 0.6f);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "dirLight.specular"), 0.3f, 0.3f, 0.3f);
+
+
+		// Point light 1
+		glm::vec3 lightColor;
+		lightColor.x = abs(sin(glfwGetTime() * Light1.x));
+		lightColor.y = abs(sin(glfwGetTime() * Light1.y));
+		lightColor.z = sin(glfwGetTime() * Light1.z);
+
+
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[0].position"), pointLightPositions[0].x, pointLightPositions[0].y, pointLightPositions[0].z);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[0].ambient"), lightColor.x, lightColor.y, lightColor.z);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[0].diffuse"), lightColor.x, lightColor.y, lightColor.z);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[0].specular"), 1.0f, 0.2f, 0.2f);
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[0].constant"), 1.0f);
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[0].linear"), 0.045f);
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[0].quadratic"), 0.075f);
+
+
+		// SpotLight
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "spotLight.position"), camera.GetPosition().x, camera.GetPosition().y, camera.GetPosition().z);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "spotLight.direction"), camera.GetFront().x, camera.GetFront().y, camera.GetFront().z);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "spotLight.ambient"), 0.2f, 0.2f, 0.8f);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "spotLight.diffuse"), 0.2f, 0.2f, 0.8f);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "spotLight.specular"), 0.0f, 0.0f, 0.0f);
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "spotLight.constant"), 1.0f);
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "spotLight.linear"), 0.3f);
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "spotLight.quadratic"), 0.7f);
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "spotLight.cutOff"), glm::cos(glm::radians(12.0f)));
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "spotLight.outerCutOff"), glm::cos(glm::radians(18.0f)));
+
+
+		// Set material properties
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 5.0f);
+
+		// Create camera transformations
+		glm::mat4 view;
+		view = camera.GetViewMatrix();
+
+		// Get the uniform locations
+		GLint modelLoc = glGetUniformLocation(lightingShader.Program, "model");
+		GLint viewLoc = glGetUniformLocation(lightingShader.Program, "view");
+		GLint projLoc = glGetUniformLocation(lightingShader.Program, "projection");
+
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+
+		glm::mat4 model(1);
+
+
+
+		//Carga de modelo 
+		view = camera.GetViewMatrix();
+		model = glm::mat4(1);
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		ran.Draw(lightingShader);
+
+		// Computadora (rotación eje Y)
+		glm::mat4 modelCompu = glm::mat4(1);
+		modelCompu = glm::translate(modelCompu, compuPos);
+		modelCompu = glm::rotate(modelCompu, glm::radians(compuRotationAngle), glm::vec3(0.0f, 0.0f, 1.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelCompu));
+		compu.Draw(lightingShader);
+
+		// Puerta (rotación eje Z)
+		glm::mat4 modelPuert = glm::mat4(1);
+		modelPuert = glm::translate(modelPuert, puertPos);
+		modelPuert = glm::rotate(modelPuert, glm::radians(puertRotationAngle), glm::vec3(0.0f, 1.0f, 0.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelPuert));
+		puert.Draw(lightingShader);
+
+		glm::mat4 modelVent = glm::mat4(1);
+		modelVent = glm::translate(modelVent, ventPos);
+		modelVent = glm::rotate(modelVent, glm::radians(ventRotationAngle), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelVent));
+		vent.Draw(lightingShader);
+
+		glm::mat4 modelVentc = glm::mat4(1);
+		modelVentc = glm::translate(modelVentc, ventcPos);
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(modelVentc));
+		ventc.Draw(lightingShader);
+
+
+
+
+		// Also draw the lamp object, again binding the appropriate shader
+		lampShader.Use();
+		// Get location objects for the matrices on the lamp shader (these could be different on a different shader)
+		modelLoc = glGetUniformLocation(lampShader.Program, "model");
+		viewLoc = glGetUniformLocation(lampShader.Program, "view");
+		projLoc = glGetUniformLocation(lampShader.Program, "projection");
+
+		glBindVertexArray(0);
+
+
+
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
+	}
+
+
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+
+
+
+	return 0;
+}
+
+// Moves/alters the camera positions based on user input
+void DoMovement()
+{
+
+	// Camera controls
+	if (keys[GLFW_KEY_W] || keys[GLFW_KEY_UP])
+	{
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+
+	}
+
+	if (keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN])
+	{
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+
+
+	}
+
+	if (keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT])
+	{
+		camera.ProcessKeyboard(LEFT, deltaTime);
+
+
+	}
+
+	if (keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT])
+	{
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+
+
+	} 
+	if (keys[GLFW_KEY_Q]) {
+		camera.ProcessMouseMovement(-100.0f * deltaTime, 0.0f); // gira izquierda
+	}
+	if (keys[GLFW_KEY_E]) {
+		camera.ProcessMouseMovement(100.0f * deltaTime, 0.0f);  // gira derecha
+	}
+
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (GLFW_KEY_ESCAPE == key && GLFW_PRESS == action)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			keys[key] = true;
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			keys[key] = false;
+		}
+	}
+
+	if (keys[GLFW_KEY_SPACE])
+	{
+		active = !active;
+		if (active)
+		{
+			Light1 = glm::vec3(0.2f, 0.8f, 1.0f);
+
+		}
+		else
+		{
+			Light1 = glm::vec3(0);//Cuado es solo un valor en los 3 vectores pueden dejar solo una componente
+		}
+	}
+	if (key == GLFW_KEY_V && action == GLFW_PRESS) {
+		// Si no se está moviendo ni regresando, empieza a moverse a la derecha
+		if (!ventcMovingRight && !ventcReturning) {
+			ventcMovingRight = true;
+		}
+	}
+	if (key == GLFW_KEY_M && action == GLFW_PRESS) {
+		if (!ventRotatingForward && !ventRotatingBackward) {
+			ventRotatingForward = true;  // inicia rotación hacia 90°
+		}
+	}
+	if (key == GLFW_KEY_B && action == GLFW_PRESS) { // para puerta
+		if (!puertRotatingForward && !puertRotatingBackward) {
+			puertRotatingForward = true;
+		}
+	}
+
+	if (key == GLFW_KEY_N && action == GLFW_PRESS) { // para compu
+		if (!compuRotatingForward && !compuRotatingBackward) {
+			compuRotatingForward = true;
+		}
+	}
+}
+void Animation() {
+	if (ventcMovingRight) {
+		if (ventcPos.x < ventcMaxMove) {
+			ventcPos.x += ventcSpeed;
+		}
+		else {
+			ventcMovingRight = false;
+			ventcReturning = true;
+		}
+	}
+	else if (ventcReturning) {
+		if (ventcPos.x > 0.0f) {
+			ventcPos.x -= ventcSpeed;
+		}
+		else {
+			ventcReturning = false;
+		}
+	}
+	// Animación rotación vent (ida y vuelta)
+	if (ventRotatingForward) {
+		if (ventRotationAngle < ventRotationMax) {
+			ventRotationAngle += ventRotationSpeed;
+			if (ventRotationAngle >= ventRotationMax) {
+				ventRotationAngle = ventRotationMax;
+				ventRotatingForward = false;
+				ventRotatingBackward = true;
+			}
+		}
+	}
+	else if (ventRotatingBackward) {
+		if (ventRotationAngle > 0.0f) {
+			ventRotationAngle -= ventRotationSpeed;
+			if (ventRotationAngle <= 0.0f) {
+				ventRotationAngle = 0.0f;
+				ventRotatingBackward = false;
+			}
+		}
+	}
+	if (ventRotatingForward) {
+		if (ventRotationAngle < ventRotationMax) {
+			ventRotationAngle += ventRotationSpeed;
+			if (ventRotationAngle >= ventRotationMax) {
+				ventRotationAngle = ventRotationMax;
+				ventRotatingForward = false;
+				ventRotatingBackward = true;
+			}
+		}
+	}
+	else if (ventRotatingBackward) {
+		if (ventRotationAngle > 0.0f) {
+			ventRotationAngle -= ventRotationSpeed;
+			if (ventRotationAngle <= 0.0f) {
+				ventRotationAngle = 0.0f;
+				ventRotatingBackward = false;
+			}
+		}
+	}
+
+	// Animación puert (rotación)
+	if (puertRotatingForward) {
+		if (puertRotationAngle > -puertRotationMax) {
+			puertRotationAngle -= puertRotationSpeed;
+			if (puertRotationAngle <= -puertRotationMax) {
+				puertRotationAngle = -puertRotationMax;
+				puertRotatingForward = false;
+				puertRotatingBackward = true;
+			}
+		}
+	}
+	else if (puertRotatingBackward) {
+		if (puertRotationAngle < 0.0f) {
+			puertRotationAngle += puertRotationSpeed;
+			if (puertRotationAngle >= 0.0f) {
+				puertRotationAngle = 0.0f;
+				puertRotatingBackward = false;
+			}
+		}
+	}
+
+	// Animación compu (rotación)
+	if (compuRotatingForward) {
+		if (compuRotationAngle > -compuRotationMax) {
+			compuRotationAngle -= compuRotationSpeed;  // resta para -90
+			if (compuRotationAngle <= -compuRotationMax) {
+				compuRotationAngle = -compuRotationMax;
+				compuRotatingForward = false;
+				compuRotatingBackward = true;
+			}
+		}
+	}
+	else if (compuRotatingBackward) {
+		if (compuRotationAngle < 0.0f) {
+			compuRotationAngle += compuRotationSpeed;  // suma para volver a 0
+			if (compuRotationAngle >= 0.0f) {
+				compuRotationAngle = 0.0f;
+				compuRotatingBackward = false;
+			}
+		}
+	}
+
+
+}
+
+void MouseCallback(GLFWwindow* window, double xPos, double yPos)
+{
+	if (firstMouse)
+	{
+		lastX = xPos;
+		lastY = yPos;
+		firstMouse = false;
+	}
+
+	GLfloat xOffset = xPos - lastX;
+	GLfloat yOffset = lastY - yPos;  // Reversed since y-coordinates go from bottom to left
+
+	lastX = xPos;
+	lastY = yPos;
+
+	camera.ProcessMouseMovement(xOffset, yOffset);
+}
